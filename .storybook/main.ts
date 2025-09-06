2603e12dfe4e5a98e4698b89bba10095ce6a866e
Amit Kumar Saini <amitsaini8385@gmial.com>
2025-09-06 22:50:22 +0530

    fix errors for project and storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from "@storybook/nextjs-vite";
+import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   "stories": [
@@ -12,11 +12,11 @@ const config: StorybookConfig = {
     "@storybook/addon-vitest"
   ],
   "framework": {
-    "name": "@storybook/nextjs-vite",
+    "name": "@storybook/nextjs",
     "options": {}
   },
   "staticDirs": [
-    "..\\public"
+    "../public"
   ]
 };
 export default config;
