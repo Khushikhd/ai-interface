7a59f308bc8bb4657979e5501ac70c8bd604fae9
khushi khandal <khushikhandal651@gmail.com>
2025-09-07 10:54:20 +0530

    updated

--- stories/ModelSelector.stories.tsx
+++ stories/ModelSelector.stories.tsx
@@ -10,16 +10,12 @@ const meta: Meta<typeof ModelSelector> = {
 export default meta;
 
 type Story = StoryObj<typeof ModelSelector>;
-
-// Ye tumhara original story hai
 export const Default: Story = {
   render: () => {
-    // tumhari original component logic yahan hogi
     return <ModelSelector model="gpt-3.5-turbo" setModel={() => {}} />;
   },
 };
 
-// Aur ye ek simple test story hai, jisse sirf test karna hai ki storybook chal raha hai ya nahi
 export const SimpleTest = {
   render: () => <div>Hello Storybook!</div>,
 };
